a1e43ac17d2febe321d364a3b380ea7572dfbffc
jeronimo <jeroj0728@gmail.com>
2026-09-11 10:58:35 -0500

    Ejercicio Termometro

---
+++ Talleres/Taller01/main.java
@@ -0,0 +1,57 @@
+public class main {
+
+    public static void main(String[] args) {
+
+        thermometer thermometer = new thermometer(20);
+
+        System.out.println("Temperatura actual: "
+                + thermometer.getTemperature() + " °C");
+
+        boolean result = thermometer.setTemperature(30);
+
+        if (result) {
+            System.out.println("Temperatura aceptada.");
+        } else {
+            System.out.println("Temperatura rechazada.");
+        }
+
+        System.out.println("Temperatura actual: "
+                + thermometer.getTemperature() + " °C");
+
+
+        result = thermometer.setTemperature(120);
+
+        if (result) {
+            System.out.println("Temperatura aceptada.");
+        } else {
+            System.out.println("Temperatura rechazada.");
+        }
+
+        System.out.println("Temperatura actual: "
+                + thermometer.getTemperature() + " °C");
+
+
+        result = thermometer.setTemperature(-40);
+
+        if (result) {
+            System.out.println("Temperatura aceptada.");
+        } else {
+            System.out.println("Temperatura rechazada.");
+        }
+
+        System.out.println("Temperatura actual: "
+                + thermometer.getTemperature() + " °C");
+
+
+        result = thermometer.setTemperature(-60);
+
+        if (result) {
+            System.out.println("Temperatura aceptada.");
+        } else {
+            System.out.println("Temperatura rechazada.");
+        }
+
+        System.out.println("Temperatura actual: "
+                + thermometer.getTemperature() + " °C");
+    }
+}
---
+++ Talleres/Taller01/thermometer.java
@@ -0,0 +1,32 @@
+/*Se requiere representar un termómetro digital que mantiene la temperatura actual (temperature) 
+expresada en grados Celsius. El dispositivo solamente puede registrar temperaturas entre -50 °C y 100 °C.
+La temperatura puede ser consultada en cualquier momento. Cuando se intenta registrar una nueva temperatura, 
+el dispositivo debe verificar que se encuentre dentro del rango permitido. 
+Si el valor no es válido, la temperatura almacenada debe conservarse sin modificaciones. */
+
+public class thermometer {
+
+    private double temperature;
+
+    public thermometer(double temperature) {
+        if (temperature >= -50 && temperature <= 100) {
+            this.temperature = temperature;
+        } else {
+            this.temperature = 0;
+        }
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public boolean setTemperature(double nuevaTemperatura) {
+
+        if (nuevaTemperatura >= -50 && nuevaTemperatura <= 100) {
+            temperature = nuevaTemperatura;
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
